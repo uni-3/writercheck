258df79d13e431a948d01d670e772a2a7aedfd46
uni <7pluto8@gmail.com>
2022-07-27 14:16:44 +0000

    refactor

--- writercheck.go
+++ writercheck.go
@@ -2,7 +2,6 @@ package writercheck
 
 import (
 	"go/ast"
-	"go/types"
 
 	"golang.org/x/tools/go/analysis"
 	"golang.org/x/tools/go/analysis/passes/inspect"
@@ -29,85 +28,67 @@ func run(pass *analysis.Pass) (interface{}, error) {
 	//ast.Print(nil, pass.Files)
 	//fmt.Println("types", pass.TypesInfo)
 	inspect.Preorder(nodeFilter, func(n ast.Node) {
-		switch n := n.(type) {
-		case *ast.Ident:
-			if n.Name == "Gopher" {
-				pass.Reportf(n.Pos(), "name of identifier must not be 'Gopher'")
+		fn := n.(*ast.FuncDecl)
+		if fn.Name.Name == "Write" {
+			// 引数
+			if fn.Type.Params.NumFields() != 1 {
+				pass.Reportf(fn.Pos(), "%s arg length '%d' must be 1", fn.Name.Name, fn.Type.Params.NumFields())
 			}
-		case *ast.FuncDecl:
-			if n.Name.Name == "Write" {
-				// 引数
-				if n.Type.Params.NumFields() != 1 {
-					pass.Reportf(n.Pos(), "%s arg length '%d' must be 1", n.Name.Name, n.Type.Params.NumFields())
+			for _, fi := range fn.Type.Params.List {
+				if fi.Names[0].Name != "p" {
+					pass.Reportf(fn.Pos(), "%s's an argument name is '%s' must be 'p'", fn.Name.Name, fi.Names[0].Name)
 				}
-				for _, fi := range n.Type.Params.List {
-					if fi.Names[0].Name != "p" {
-						pass.Reportf(n.Pos(), "%s's an argument name is '%s' must be 'p'", n.Name.Name, fi.Names[0].Name)
-					}
-					switch ft := fi.Type.(type) {
-					case *ast.ArrayType:
-						switch et := ft.Elt.(type) {
-						case *ast.Ident:
-							if et.Name != "byte" {
-								pass.Reportf(n.Pos(), "%s arg is invalid type '%s' must be 'byte'", fi.Names[0].Name, et.Name)
-							}
-						default:
-							pass.Reportf(n.Pos(), "%s arg is invalid", fi.Names[0].Name)
+				switch ft := fi.Type.(type) {
+				case *ast.ArrayType:
+					switch et := ft.Elt.(type) {
+					case *ast.Ident:
+						if et.Name != "byte" {
+							pass.Reportf(fn.Pos(), "%s arg is invalid type '%s' must be 'byte'", fi.Names[0].Name, et.Name)
 						}
-
 					default:
-						pass.Reportf(n.Pos(), "%s arg is invalid", fi.Names[0].Name)
+						pass.Reportf(fn.Pos(), "%s arg is invalid", fi.Names[0].Name)
 					}
-				}
 
-				// 返り値
-				results := n.Type.Results
-				if results.NumFields() != 2 {
-					pass.Reportf(n.Pos(), "%s arg length '%d' must be 1", n.Name.Name, results.NumFields())
-					return
-				}
-
-				resInt := results.List[0]
-				// 1つめ
-				if resInt.Names[0].Name != "n" {
-					pass.Reportf(n.Pos(), "%s's an argument name is '%s' must be 'n'", n.Name.Name, resInt.Names[0].Name)
-				}
-				switch typ := resInt.Type.(type) {
-				case *ast.Ident:
-					if typ.Name != "int" {
-						pass.Reportf(n.Pos(), "%s arg is invalid type '%s' must be 'int'", resInt.Names[0].Name, typ.Name)
-					}
 				default:
-					pass.Reportf(n.Pos(), "%s arg is invalid", resInt.Names[1].Name)
+					pass.Reportf(fn.Pos(), "%s arg is invalid", fi.Names[0].Name)
 				}
+			}
 
-				resErr := results.List[1]
-				// 2つめ
-				if resErr.Names[0].Name != "err" {
-					pass.Reportf(n.Pos(), "%s's an argument name is '%s' must be 'p'", n.Name.Name, resErr.Names[0].Name)
+			// 返り値
+			results := fn.Type.Results
+			if results.NumFields() != 2 {
+				pass.Reportf(fn.Pos(), "%s arg length '%d' must be 1", fn.Name.Name, results.NumFields())
+				return
+			}
+
+			resInt := results.List[0]
+			// 1つめ
+			if resInt.Names[0].Name != "n" {
+				pass.Reportf(fn.Pos(), "%s's an argument name is '%s' must be 'n'", fn.Name.Name, resInt.Names[0].Name)
+			}
+			switch typ := resInt.Type.(type) {
+			case *ast.Ident:
+				if typ.Name != "int" {
+					pass.Reportf(fn.Pos(), "%s arg is invalid type '%s' must be 'int'", resInt.Names[0].Name, typ.Name)
 				}
-				switch typ := resErr.Type.(type) {
-				case *ast.Ident:
-					if typ.Name != "error" {
-						pass.Reportf(n.Pos(), "%s arg is invalid type '%s' must be 'byte'", resErr.Names[0].Name, typ.Name)
-					}
-				default:
-					pass.Reportf(n.Pos(), "%s arg is invalid", resErr.Names[0].Name)
+			default:
+				pass.Reportf(n.Pos(), "%s arg is invalid", resInt.Names[1].Name)
+			}
+
+			resErr := results.List[1]
+			if resErr.Names[0].Name != "err" {
+				pass.Reportf(fn.Pos(), "%s's an argument name is '%s' must be 'p'", fn.Name.Name, resErr.Names[0].Name)
+			}
+			switch typ := resErr.Type.(type) {
+			case *ast.Ident:
+				if typ.Name != "error" {
+					pass.Reportf(fn.Pos(), "%s arg is invalid type '%s' must be 'byte'", resErr.Names[0].Name, typ.Name)
 				}
+			default:
+				pass.Reportf(fn.Pos(), "%s arg is invalid", resErr.Names[0].Name)
 			}
 		}
 	})
 
 	return nil, nil
-}
-
-type foundFact struct{}
-
-func (*foundFact) String() string { return "found" }
-func (*foundFact) AFact()         {}
-
-var errTyp = types.Universe.Lookup("error").Type().Underlying().(*types.Interface)
-
-func isErrType(t types.Type) bool {
-	return types.Implements(t, errTyp)
 }
